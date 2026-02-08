1f43f0de245d11d544162cfb6046d0d00738e93c
Cookietank <67625970+Cookietank@users.noreply.github.com>
2026-02-08 11:16:30 +0000

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://soundcloud.com/bad-panda-records/jauge-gather3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dguaudmbm/video/upload/v1770549348/Jauge_-_Gather-210403261_jsbzcz.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
